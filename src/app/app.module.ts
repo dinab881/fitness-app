92465ee0a2ecc52faa4a8fd28a144f34f0087701
user-rq <dina.bolshakova@requestum.com>
2019-01-11 17:11:44 +0200

    Add store na styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,3 +17,15 @@ import {Store} from 'store';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/**
+ var config = {
+    apiKey: "AIzaSyDAXuPY1-xkg8zocsGBQd_orIjaptgs5fI",
+    authDomain: "fitnessapp-64dc4.firebaseapp.com",
+    databaseURL: "https://fitnessapp-64dc4.firebaseio.com",
+    projectId: "fitnessapp-64dc4",
+    storageBucket: "",
+    messagingSenderId: "224799351924"
+  };
+ firebase.initializeApp(config);
+ */
